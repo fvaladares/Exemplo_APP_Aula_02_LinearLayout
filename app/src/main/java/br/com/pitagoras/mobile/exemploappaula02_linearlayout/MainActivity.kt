ed74791bc0c865ce67f7e2bd31c371886c797e3b
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-09-03 20:56:18 -0300

    Inclusão do viewBinding Exemplo de uso do scrool view
    Inserção de viewgrous aninhados;

--- app/src/main/java/br/com/pitagoras/mobile/exemploappaula02_linearlayout/MainActivity.kt
+++ app/src/main/java/br/com/pitagoras/mobile/exemploappaula02_linearlayout/MainActivity.kt
@@ -2,33 +2,50 @@ package br.com.pitagoras.mobile.exemploappaula02_linearlayout
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import br.com.pitagoras.mobile.exemploappaula02_linearlayout.databinding.ActivityMainBinding
 
 
-//TODO: Impedir que envie um nome vazio
-//TODO: Criar um botão para limpar o campo de nome completo e o textview que exibe o resultado
-//TODO: Inserir um imageView no cabeçalho
-//TODO: Extra: colocar um botao ao lado do outro. Dica: Utilize outro viewGroup para organizar os botoes
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         iniciarComponentes()
-
     }
 
     private fun iniciarComponentes() {
-        val tvNomeCompleto = findViewById<TextView>(R.id.tvMainNome)
+        // TODO - substituir e usar o viewBinding
         val btEnviar = findViewById<Button>(R.id.btMainEnviar)
-        val etNomeCompleto = findViewById<EditText>(R.id.etMainNomeCompleto)
+        val btLimpar = findViewById<Button>(R.id.btMainLimparCampos)
+
+        //TODO Exibir um toast no lugar do texto no TextView
         btEnviar.setOnClickListener {
-            val nomeCompleto = "O nome informado foi: ${etNomeCompleto.text}"
-            tvNomeCompleto.text = nomeCompleto
+            exibirMensagem()
         }
+
+        btLimpar.setOnClickListener {
+            binding.etMainNomeCompleto.text.clear()
+            binding.tvMainNome.text = ""
+        }
+
+
         // Como exibir uma mensagem temporária na tela. :)
         Toast.makeText(this, "Exemplo", Toast.LENGTH_LONG).show()
     }
+
+    private fun exibirMensagem() {
+        var textoDoTextView = getString(R.string.mensagem_erro_nome_nao_preenchido)
+        if (!binding.etMainNomeCompleto.text.isNullOrBlank()) {
+            textoDoTextView = getString(R.string.mensagem_nome_ok, binding.etMainNomeCompleto.text)
+        }
+        binding.tvMainNome.text = textoDoTextView
+    }
+
+
 }
